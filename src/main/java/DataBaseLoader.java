8ad0da8c3b2b5eedc6bedcfd7dd41dd7bca42c7a
JoMedeiros <josivan.medeiros@live.com>
2019-09-11 15:38:16 -0300

    the last hope

--- src/main/java/DataBaseLoader.java
+++ src/main/java/DataBaseLoader.java
@@ -20,7 +20,7 @@ public class DataBaseLoader implements Runnable {
             line = bufferedReader.readLine();
             String[] cells = line.split(";");
             // Read values
-            int numLines = 200_000;
+            int numLines = 200000;
             line = bufferedReader.readLine();
             //while( line != null ) {
             for (int i = 0; i < numLines; i++) {
