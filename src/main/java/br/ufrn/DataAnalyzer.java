99a0e60e7cac3bdafb4eeafe4358887d8f8237a8
Josivan Medeiros da Silva Gois <josivanmedeiros@A307-04L.imd.local>
2019-10-02 18:06:10 -0300

    savepoint(it doesnt work at this point)

--- src/main/java/br/ufrn/DataAnalyzer.java
+++ src/main/java/br/ufrn/DataAnalyzer.java
@@ -2,12 +2,14 @@ package br.ufrn;
 
 import java.util.concurrent.RecursiveAction;
 import java.util.function.Consumer;
+import java.util.concurrent.Flow;
 import java.util.stream.Stream;
 
-public class DataAnalyzer extends RecursiveAction {
+public class DataAnalyzer extends RecursiveAction implements Flow.Subscriber {
     private static final int SEQUENTIAL_THRESHOLD = 100000;
-    DataBase data;
-    Result result;
+    private DataBase data;
+    private Result result;
+    private Flow.Subscription subscription;
     public DataAnalyzer(DataBase dataBase, Result result){
         this.data = dataBase;
         this.result = result;
@@ -55,29 +57,6 @@ public class DataAnalyzer extends RecursiveAction {
             }
         };
         this.data.parallelStream().forEach(gerarResultado);
-//        while (!data.isFinished()) {
-//            String [] line = data.getLine();
-//            // Respostas
-//            String TX_RESPOSTAS_CN = line[94];
-//            String TX_RESPOSTAS_CH = line[95];
-//            String TX_RESPOSTAS_LC = line[96];
-//            String TX_RESPOSTAS_MT = line[97];
-//            //String TP_LINGUA = line[98];
-//            // Gabaritos:
-//            String TX_GABARITO_CN = line[99];
-//            String TX_GABARITO_CH = line[100];
-//            String TX_GABARITO_LC = line[101];
-//            String TX_GABARITO_MT = line[102];
-//            // Resultado
-//            try {
-//                gerarResultado(TX_RESPOSTAS_CN, TX_GABARITO_CN, "CN");
-//                gerarResultado(TX_RESPOSTAS_CH, TX_GABARITO_CH, "CH");
-//                gerarResultado(TX_RESPOSTAS_LC, TX_GABARITO_LC, "LC");
-//                gerarResultado(TX_RESPOSTAS_MT, TX_GABARITO_MT, "MT");
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//        }
     }
 
     private void gerarResultado(String respostas, String gabarito, String prova) throws InterruptedException {
@@ -94,5 +73,26 @@ public class DataAnalyzer extends RecursiveAction {
             }
             this.result.putResult(resultado, prova);
         }
+    }
+
+    @Override
+    public void onSubscribe(Flow.Subscription subscription) {
+        this.subscription = subscription;
+        this.subscription.request(1);
+    }
+
+    @Override
+    public void onNext(Object item) {
+        this.subscription.request(1);
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+        throwable.printStackTrace();
+    }
+
+    @Override
+    public void onComplete() {
+        //this.subscription.cancel();
     }
 }
